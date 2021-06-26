d9fbf7d3dbbaaf687af7815df100c7a0e41c0127
Sagun Karki <sagunkarki111@gmail.com>
2021-06-26 16:33:59 -0400

    js

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ document.querySelector(".button-container").addEventListener("click", () =>{
     let text = document.getElementById("filter-jobs").value;
    
 })
+
+funtion getJobs(){
+    return fetch("data.json")
+    .then(respone => response.json())
+    .then(data => {
+        
+        return data
+    })
+}
+
+funtion showJobs(jobs){
+    console.log(jobs);
+
+}
+getJobs().then(data => {
+    showJobs(data);
+});
